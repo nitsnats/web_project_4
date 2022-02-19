7e1c0e3af2b6c4407b6fd811d64c686bc955eec8
Nitsa Saban <nitsnats@hotmail.com>
2022-02-19 18:04:14 +0200

    fixing submit event

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,38 @@
 const profileName = document.querySelector(".profile__name_type_name");
-const profileProfession = document.querySelector(
-  ".profile__profession_type_profession"
-);
+const profileProfession = document.querySelector(".profile__profession_type_profession");
 const editButtonProfile = document.querySelector(".profile__button");
 const popup = document.querySelector(".popup");
+const form = document.querySelector(".popup__form");
 
 const closeButtonPopup = popup.querySelector(".popup__close");
 const popupInputName = popup.querySelector(".popup__input_type_name");
-const popupInputProfession = popup.querySelector(
-  ".popup__input_type_profession"
-);
+const popupInputProfession = popup.querySelector(".popup__input_type_profession");
 const saveButtonPopup = popup.querySelector(".popup__save");
 
-editButtonProfile.addEventListener("click", () => {
+
+function openPopup() {
+  popup.classList.add("popup__opened");
+}
+
+function closePopup() {
+  popup.classList.remove("popup__opened");
+}
+
+function edit(){
   popupInputName.value = profileName.textContent;
   popupInputProfession.value = profileProfession.textContent;
   openPopup();
-});
+}
 
-saveButtonPopup.addEventListener("click", (e) => {
+function save(e){
   e.preventDefault();
   profileName.textContent = popupInputName.value;
   profileProfession.textContent = popupInputProfession.value;
   closePopup();
-});
+}
+
 
 closeButtonPopup.addEventListener("click", closePopup);
-
-function openPopup() {
-  popup.classList.add("popup__open");
-}
-
-function closePopup() {
-  popup.classList.remove("popup__open");
-}
+form.addEventListener("submit", save); 
+editButtonProfile.addEventListener("click", openPopup);
+ 
